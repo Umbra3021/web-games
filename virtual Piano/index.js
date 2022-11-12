cf945cbcbe5bba5f4a51f82fea26e546d73981f0
Abhishek Sengupta <chotuame@gmail.com>
2022-11-13 02:46:46 +0530

    final.

--- virtual Piano/index.js
+++ virtual Piano/index.js
@@ -16,78 +16,82 @@ document.addEventListener("keydown",function(event){
    
 })
 
+
+
+
+
 function sound(key){
 
     switch (key) {
         case 'A':
         case 'a':
-            var A_major = new Audio('sound/crash.mp3');
+            var A_major = new Audio('sound/A.mp3');
             A_major.play();
             break;
         
         case 'B':
         case 'b':
-            var B_major = new Audio('sound/B.Wav');
+            var B_major = new Audio('sound/B.mp3');
             B_major.play();
             break;
     
         case 'C':
         case 'c':
-            var C_major = new Audio('sound/C.Wav');
+            var C_major = new Audio('sound/C.mp3');
             C_major.play();
             break;
 
         case 'D':
         case 'd':
-            var D_major = new Audio('sound/D.Wav');
+            var D_major = new Audio('sound/D.mp3');
             D_major.play();
             break;
 
         case 'E':
         case 'e':
-            var E_major = new Audio('sound/E.Wav');
+            var E_major = new Audio('sound/E.mp3');
             E_major.play();
             break;
         
         case 'F':
         case 'f':
-            var F_major = new Audio('sound/F.Wav');
+            var F_major = new Audio('sound/F.mp3');
             F_major.play();
             break;
 
         case 'G':
         case 'g':
-            var G_major = new Audio('sound/G.Wav');
+            var G_major = new Audio('sound/G.mp3');
             G_major.play();
             break;
 
         case'Ab':
         case 'k':
-            var Ab = new Audio('sound/Ab.Wav');
+            var Ab = new Audio('sound/Ab.mp3');
             Ab.play();
             break;
 
         case'Bb':
         case 'l':
-            var Bb = new Audio('sound/Bb.Wav');
+            var Bb = new Audio('sound/Bb.mp3');
             Bb.play();
             break;
 
         case 'Db':
         case 'i':
-            var Db = new Audio('sound/Db.Wav');
+            var Db = new Audio('sound/Db.mp3');
             Db.play();
             break;
 
         case 'Eb':
         case 'o':
-            var Eb = new Audio('sound/Eb.Wav');
+            var Eb = new Audio('sound/Eb.mp3');
             Eb.play();
             break;
 
         case 'Gb':
         case 'j':
-            var Gb = new Audio('sound/Gb.Wav');
+            var Gb = new Audio('sound/Gb.mp3');
             Gb.play();
             break;
 
@@ -95,6 +99,3 @@ function sound(key){
             break;
     }
 }
-
-
-
